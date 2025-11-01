d3d8dd41fbb0d41a954c99c852979b952c82def8
Laolu02 <emmakamz224@gmail.com>
2025-11-01 09:55:52 +0100

    0.1.2

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -1,33 +1,34 @@
-// 'use client'
+'use client'
 
-// import { useParams } from 'next/navigation'
-// import { useSession } from 'next-auth/react'
-// import { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import ChatArea from '@/components/ChatArea'
 
 
-// export default function GroupChatPage() {
-//   const params = useParams()
-//   const { data: session } = useSession()
-//   const [group, setGroup] = useState(null)
+export default function GroupChatPage() {
+  const params = useParams()
+  const { data: session } = useSession()
+  const [group, setGroup] = useState(null)
 
-//   useEffect(() => {
-//     if (params.id) {
-//       fetchGroup()
-//     }
-//   }, [params.id])
+  useEffect(() => {
+    if (params.id) {
+      fetchGroup()
+    }
+  }, [params.id])
 
-//   const fetchGroup = async () => {
-//     const res = await fetch(`/api/groups/${params.id}`)
-//     const data = await res.json()
-//     setGroup(data.group)
-//   }
+  const fetchGroup = async () => {
+    const res = await fetch(`/api/groups/${params.id}`)
+    const data = await res.json()
+    setGroup(data.group)
+  }
 
-//   if (!session || !group) return <div>Loading...</div>
+  if (!session || !group) return <div>Loading...</div>
 
-//   return (
-//     <GroupChat
-//       currentUser={session.user}
-//       group={group}
-//     />
-//   )
-// }
+  return (
+    <ChatArea
+      currentUser={session.user}
+      group={group}
+    />
+  )
+}
